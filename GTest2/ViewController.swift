5592217606294ec79bc8e538957fe12b673503f2
Carnell Johnson <CDAndreJohnson@Carnells-MacBook-Air.local>
2017-01-14 15:16:31 -0500

    Initial Commit

---
+++ GTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GTest2
+//
+//  Created by Carnell Johnson on 1/14/17.
+//  Copyright © 2017 Carnell Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
